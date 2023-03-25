30e79c7a31d0f0b7208555176eb196b528aa4566
Oona Singh <oonarsingh@gmail.com>
2023-03-25 12:56:17 +0400

    controller

--- app/controllers/ceo_controller.rb
+++ app/controllers/ceo_controller.rb
@@ -1,10 +1,17 @@
-require 'open-uri'
+require 'uri'
+require 'net/http'
 require 'nokogiri'
 
 class CeoController < ApplicationController
   def index
-    url = 'https://www.example.com/about'
-    doc = Nokogiri::HTML(open(url))
+    url = URI.parse('https://www.example.com/about')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true if url.scheme == 'https'
+
+    request = Net::HTTP::Get.new(url.request_uri)
+    response = http.request(request)
+
+    doc = Nokogiri::HTML(response.body)
 
     ceo_name = doc.at_css('div.ceo-name').text
     ceo_email = doc.at_css('div.ceo-email').text
