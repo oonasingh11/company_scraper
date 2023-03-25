ec2b2324962f2ac9c3e5ad0cc55735561169e6e9
Oona Singh <oonarsingh@gmail.com>
2023-03-25 12:54:50 +0400

    scraping logic

--- app/controllers/ceo_controller.rb
+++ app/controllers/ceo_controller.rb
@@ -1,2 +1,19 @@
+require 'open-uri'
+require 'nokogiri'
+
 class CeoController < ApplicationController
+  def index
+    url = 'https://www.example.com/about'
+    doc = Nokogiri::HTML(open(url))
+
+    ceo_name = doc.at_css('div.ceo-name').text
+    ceo_email = doc.at_css('div.ceo-email').text
+    ceo_phone = doc.at_css('div.ceo-phone').text
+
+    @ceo_info = {
+      name: ceo_name,
+      email: ceo_email,
+      phone: ceo_phone
+    }
+  end
 end
