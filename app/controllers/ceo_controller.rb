d219ac2cc2513ce0d1d9b2bb4ecff757855a1725
Oona Singh <oonarsingh@gmail.com>
2023-03-27 11:01:18 +0400

    controller modified, didn't take URL params

--- app/controllers/ceo_controller.rb
+++ app/controllers/ceo_controller.rb
@@ -4,26 +4,29 @@ require 'nokogiri'
 
 class CeoController < ApplicationController
   def index
-    url = URI.parse('https://www.example.com/about')
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true if url.scheme == 'https'
+    url = params[:url]
+    if url.present?
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if uri.scheme == 'https'
 
-    request = Net::HTTP::Get.new(url.request_uri)
-    response = http.request(request)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
 
-    doc = Nokogiri::HTML(response.body)
+      doc = Nokogiri::HTML(response.body)
 
-    ceo_name = doc.at_css('div.ceo-name')&.text || 'Unknown'
-    ceo_email = doc.at_css('div.ceo-email')&.text || 'Unknown'
-    ceo_phone = doc.at_css('div.ceo-phone')&.text || 'Unknown'
+      ceo_name = doc.at_css('div.ceo-name')&.text || 'Unknown'
+      ceo_email = doc.at_css('div.ceo-email')&.text || 'Unknown'
+      ceo_phone = doc.at_css('div.ceo-phone')&.text || 'Unknown'
 
-    @ceo_info = {
-      name: ceo_name,
-      email: ceo_email,
-      phone: ceo_phone
-    }
+      @ceo_info = {
+        name: ceo_name,
+        email: ceo_email,
+        phone: ceo_phone
+      }
 
-    @ceo = Ceo.new(name: ceo_name, email: ceo_email, phone: ceo_phone)
-    @ceo.save
+      @ceo = Ceo.new(name: ceo_name, email: ceo_email, phone: ceo_phone)
+      @ceo.save
+    end
   end
 end
